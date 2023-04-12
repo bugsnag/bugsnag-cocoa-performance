2fc9e211ee3dfee5d7fd4de48f0643eedb1beef0
Robert <robert.smartbear@gmail.com>
2023-04-12 11:08:43 +0200

    Set TestApp deployment target to iOS 11.0

---
+++ BugsnagPerformanceTestsApp/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  BugsnagPerformanceTestsApp
+//
+//  Created by Robert B on 12/04/2023.
+//  Copyright © 2023 Bugsnag. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
