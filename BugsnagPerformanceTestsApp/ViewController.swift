cf2ddf0b48bc03bdc143fb44d396d23c13f8f38b
robert-smartbear <126675445+robert-smartbear@users.noreply.github.com>
2023-04-18 10:26:21 +0200

    [PLAT-9901] Added micro-benchmark performance tests (#93)
    * Implemented micro-benchmark performance tests
    
    ---------
    
    Co-authored-by: Robert <robert.smartbear@gmail.com>

---
+++ BugsnagPerformanceTestsApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BugsnagPerformanceTestsApp
+//
+//  Created by Robert B on 12/04/2023.
+//  Copyright © 2023 Bugsnag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
