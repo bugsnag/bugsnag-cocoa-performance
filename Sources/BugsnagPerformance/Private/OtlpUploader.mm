1d1571c3e0f2cabcc4483dd55363101923332513
Karl Stenerud <kstenerud@gmail.com>
2023-05-31 13:39:41 +0200

    Add subseconds to iso8601 dates even on ios 11

--- Sources/BugsnagPerformance/Private/OtlpUploader.mm
+++ Sources/BugsnagPerformance/Private/OtlpUploader.mm
@@ -31,13 +31,23 @@ void OtlpUploader::upload(OtlpPackage &package, UploadResultCallback callback) n
     auto urlRequest = [NSMutableURLRequest requestWithURL:(NSURL *)endpoint_];
     [urlRequest setValue:apiKey_ forHTTPHeaderField:@"Bugsnag-Api-Key"];
 
+    NSDate *now = [NSDate new];
+    NSString *suffix = @"";
     NSISO8601DateFormatOptions options = NSISO8601DateFormatWithInternetDateTime;
     if (@available(iOS 11.2, *)) {
         options |= NSISO8601DateFormatWithFractionalSeconds;
+    } else {
+        NSDateComponents *components = [NSCalendar.currentCalendar components:(NSCalendarUnitNanosecond) fromDate:now];
+        NSInteger msec = components.nanosecond / 1000000;
+        suffix = [NSString stringWithFormat:@".%03ldZ", (long)msec];
     }
-    NSString *timestamp = [NSISO8601DateFormatter stringFromDate:[NSDate new]
+    NSString *timestamp = [NSISO8601DateFormatter stringFromDate:now
                                                         timeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]
                                                    formatOptions:options];
+    if (suffix.length > 0) {
+        timestamp = [NSString stringWithFormat:@"%@%@", [timestamp substringToIndex:timestamp.length-1], suffix];
+    }
+
     [urlRequest setValue:timestamp forHTTPHeaderField:@"Bugsnag-Sent-At"];
     package.fillURLRequest(urlRequest);
 
