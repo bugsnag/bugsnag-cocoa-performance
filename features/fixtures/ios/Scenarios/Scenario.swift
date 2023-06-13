d3439d5063262f78a6aa6bf98b44a15634e7c903
Robert <robert.smartbear@gmail.com>
2023-06-13 21:25:39 +0200

    Log library startup times to MR

--- features/fixtures/ios/Scenarios/Scenario.swift
+++ features/fixtures/ios/Scenarios/Scenario.swift
@@ -8,11 +8,14 @@
 import BugsnagPerformance
 import Foundation
 
+typealias MazerunnerMeasurement = (name: String, metrics: [String: Any])
+
 class Scenario: NSObject {
     
     static let mazeRunnerURL = "http://bs-local.com:9339"
     
     var config = BugsnagPerformanceConfiguration.loadConfig()
+    var pendingMeasurements: [MazerunnerMeasurement] = []
     
     func configure() {
         NSLog("Scenario.configure()")
@@ -38,17 +41,60 @@ class Scenario: NSObject {
     
     func startBugsnag() {
         NSLog("Scenario.startBugsnag()")
-        BugsnagPerformance.start(configuration: config)
+        performAndReportDuration({
+            BugsnagPerformance.start(configuration: config)
+        }, measurement: "start")
     }
     
     func run() {
         NSLog("Scenario.run() has not been overridden!")
         fatalError("To be implemented by subclass")
     }
+    
+    func reportMeasurements() {
+        pendingMeasurements.forEach { measurement in
+            report(metrics: measurement.metrics, name: measurement.name)
+        }
+        pendingMeasurements = []
+    }
 
     func waitForCurrentBatch() {
         NSLog("Scenario.waitForCurrentBatch()")
         // Wait long enough to allow the current batch to be packaged and sent
         Thread.sleep(forTimeInterval: 1.0)
+    }
+    
+    func performAndReportDuration(_ body: () -> Void, measurement: String) {
+        let startDate = Date()
+        body()
+        let endDate = Date()
+        
+        let calendar = Calendar.current
+        let duration = calendar.dateComponents([.nanosecond], from: startDate, to: endDate)
+        let metrics = ["duration.nanos": "\(duration.nanosecond ?? 0)"]
+        pendingMeasurements.append((name: measurement, metrics: metrics))
+    }
+    
+    func report(metrics: [String: Any], name: String) {
+        guard let url = URL(string: "\(Scenario.mazeRunnerURL)/metrics") else {
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        var body = metrics
+        body["metric.measurement"] = name
+        body["device.manufacturer"] = "Apple"
+        body["device.model"] = UIDevice.current.model
+        body["os.name"] = UIDevice.current.systemName
+        body["os.version"] = UIDevice.current.systemVersion
+        let jsonData = try? JSONSerialization.data(withJSONObject: body)
+        guard let jsonData else {
+            return
+        }
+        request.httpBody = jsonData
+        
+        URLSession.shared.dataTask(with: request).resume()
     }
 }
