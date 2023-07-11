c20e35cc6a52cbbeb2ea22bca4f38984eb44fee4
Steve Kirkland-Walton <steve@bugsnag.com>
2023-07-11 11:17:24 +0100

    Clear data after each scenario

--- features/steps/app_steps.rb
+++ features/steps/app_steps.rb
@@ -10,12 +10,7 @@ When('I run {string}') do |scenario_name|
 end
 
 When('I invoke {string}') do |method_name|
-  Maze::Server.commands.add({ action: "invoke_method", args: [method_name] })
-  Maze.driver.click_element :execute_command
-  # Ensure fixture has read the command
-  count = 100
-  sleep 0.1 until Maze::Server.commands.remaining.empty? || (count -= 1) < 1
-  raise 'Test fixture did not GET /command' unless Maze::Server.commands.remaining.empty?
+  invoke_command method_name
 end
 
 When('I invoke {string} with parameter {string}') do |method_name, arg1|
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,12 @@
-Maze.hooks.after do |scenario|
-  step 'I invoke "clearPersistentData"'
+def invoke_command(method_name)
+  Maze::Server.commands.add({ action: "invoke_method", args: [method_name] })
+  Maze.driver.click_element :execute_command
+  # Ensure fixture has read the command
+  count = 100
+  sleep 0.1 until Maze::Server.commands.remaining.empty? || (count -= 1) < 1
+  raise 'Test fixture did not GET /command' unless Maze::Server.commands.remaining.empty?
+end
+
+Maze.hooks.after do |_scenario|
+  invoke_command 'clearPersistentData'
 end
