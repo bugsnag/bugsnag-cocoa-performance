f83f7b9b45f2a4a4e173d0e46e6e465734cefc86
Robert <robert.smartbear@gmail.com>
2023-08-24 20:47:07 +0200

    Fixes for pipeline

--- features/fixtures/ios/build.sh
+++ features/fixtures/ios/build.sh
@@ -5,20 +5,42 @@ echo "$(sw_vers -productName) $(sw_vers -productVersion) $(sw_vers -buildVersion
 
 set -euxo pipefail
 
-if [ [ $@ == *"--disableSwizzling"* ] ]; then
-    sed -i '' -e 's/DISABLE_SWIZZLING/<key>disableSwizzling</key>\n\t\t\t<true/>/' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
-else
-    sed -i '' -e 's/DISABLE_SWIZZLING//' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
-fi
+disable_swizzling_key='<key>test_key_to_replace1</key>'
+disable_swizzling_value='<string>test_value_to_replace1</string>'
+swizzling_premain_key='<key>test_key_to_replace2</key>'
+swizzling_premain_value='<string>test_value_to_replace2</string>'
 
-if [ [ $@ == *"--swizzlingPremain"* ] ]; then
-    sed -i '' -e 's/SWIZZLING_PREMAIN/<key>disableSwizzling</key>\n\t\t\t<true/>/' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
-else
-    sed -i '' -e 's/SWIZZLING_PREMAIN//' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
-fi
+fixture_name='Fixture'
+for ((i=1;i<=$#;i++));
+do
+    if [ ${!i} = '--fixtureName' ]
+    then ((i++))
+        fixture_name=${!i};
+    fi
+    if [ ${!i} = '--disableSwizzling' ]
+    then
+        disable_swizzling_key='<key>disableSwizzling</key>'
+        disable_swizzling_value='<true/>\t\t\t\t\t\t\t\t\t\t';
+    fi
+    if [ ${!i} = '--swizzlingPremain' ]
+    then
+        swizzling_premain_key='<key>swizzleViewLoadPreMain</key>'
+        swizzling_premain_value='<true/>\t\t\t\t\t\t\t\t\t'
+    fi
+done;
+
+sed -i '' -e 's|DISABLE_SWIZZLING_KEY|'$disable_swizzling_key'|' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
+sed -i '' -e 's|DISABLE_SWIZZLING_VALUE|'$disable_swizzling_value'|' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
+sed -i '' -e 's|SWIZZLING_PREMAIN_KEY|'$swizzling_premain_key'|' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
+sed -i '' -e 's|SWIZZLING_PREMAIN_VALUE|'$swizzling_premain_value'|' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
-xcodebuild -destination generic/platform=iOS -archivePath Fixture.xcarchive -scheme Fixture archive -allowProvisioningUpdates -quiet
+xcodebuild -destination generic/platform=iOS -archivePath $fixture_name.xcarchive -scheme Fixture archive -allowProvisioningUpdates -quiet
 
-xcodebuild -destination generic/platform=iOS -archivePath Fixture.xcarchive -exportArchive -exportPath output -exportOptionsPlist ExportOptions.plist -allowProvisioningUpdates -quiet
+xcodebuild -destination generic/platform=iOS -archivePath $fixture_name.xcarchive -exportArchive -exportPath output -exportOptionsPlist ExportOptions.plist -allowProvisioningUpdates -quiet
+
+sed -i '' -e 's|'$disable_swizzling_key'|DISABLE_SWIZZLING_KEY|' ./Fixture/Info.plist
+sed -i '' -e 's|'$disable_swizzling_value'|DISABLE_SWIZZLING_VALUE|' ./Fixture/Info.plist
+sed -i '' -e 's|'$swizzling_premain_key'|SWIZZLING_PREMAIN_KEY|' ./Fixture/Info.plist
+sed -i '' -e 's|'$swizzling_premain_value'|SWIZZLING_PREMAIN_VALUE|' ./Fixture/Info.plist
