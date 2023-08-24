0e9b4340053a459a1f80193ef6f1470e86627a5b
Robert <robert.smartbear@gmail.com>
2023-08-24 17:38:03 +0200

    Added E2E tests for different plist configurations

--- features/fixtures/ios/build.sh
+++ features/fixtures/ios/build.sh
@@ -5,6 +5,18 @@ echo "$(sw_vers -productName) $(sw_vers -productVersion) $(sw_vers -buildVersion
 
 set -euxo pipefail
 
+if [ [ $@ == *"--disableSwizzling"* ] ]; then
+    sed -i '' -e 's/DISABLE_SWIZZLING/<key>disableSwizzling</key>\n\t\t\t<true/>/' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
+else
+    sed -i '' -e 's/DISABLE_SWIZZLING//' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
+fi
+
+if [ [ $@ == *"--swizzlingPremain"* ] ]; then
+    sed -i '' -e 's/SWIZZLING_PREMAIN/<key>disableSwizzling</key>\n\t\t\t<true/>/' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
+else
+    sed -i '' -e 's/SWIZZLING_PREMAIN//' $(dirname "${BASH_SOURCE[0]}")/Fixture/Info.plist
+fi
+
 cd $(dirname "${BASH_SOURCE[0]}")
 
 xcodebuild -destination generic/platform=iOS -archivePath Fixture.xcarchive -scheme Fixture archive -allowProvisioningUpdates -quiet
