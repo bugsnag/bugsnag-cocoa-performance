1a16f4fb7a96d8c3ac2f1a942bc90881fdb8513f
Robert <robert.smartbear@gmail.com>
2023-09-20 16:42:49 +0200

    Updated test app

--- BugsnagPerformanceTestsApp/AppDelegate.swift
+++ BugsnagPerformanceTestsApp/AppDelegate.swift
@@ -10,8 +10,11 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
         return true
     }
 }
--- BugsnagPerformanceTestsApp/ViewController.swift
+++ BugsnagPerformanceTestsApp/ViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter = 0
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.text = "\(counter)"
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func increment() {
+        counter += 1
+        textField.text = "\(counter)"
     }
 }
 
