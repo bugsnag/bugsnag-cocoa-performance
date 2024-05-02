4b6b09becffa758d393edebdaa0d347ce56b1b9e
Karl Stenerud <kstenerud@gmail.com>
2024-05-02 13:26:10 +0200

    Fix broken tests that were uncovered by removing the 10s delay

--- features/fixtures/ios/Scenarios/AutoInstrumentNavigationViewLoadScenario.swift
+++ features/fixtures/ios/Scenarios/AutoInstrumentNavigationViewLoadScenario.swift
@@ -15,6 +15,10 @@ class AutoInstrumentNavigationViewLoadScenario: Scenario {
     override func configure() {
         super.configure()
         config.autoInstrumentViewControllers = true
+        // This test can generate a variable number of spans depending on the OS version,
+        // so use a timed send instead.
+        config.internal.autoTriggerExportOnBatchSize = 100
+        config.internal.performWorkInterval = 1
     }
     
     override func run() {
