0e9110bede7264444c3f41ee1199c8af0241a4e8
Josh Edney <j.edney17@gmail.com>
2024-11-19 09:19:47 +0000

    full CI

---
+++ .buildkite/pipeline_trigger.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+if [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
+  "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "master" ||
+  ! -z "$FULL_SCHEDULED_BUILD" ]]; then
+  echo "Running full build"
+  buildkite-agent pipeline upload .buildkite/pipeline.full.yml
+else
+  # Basic build, but allow a full build to be triggered
+  echo "Running basic build"
+  buildkite-agent pipeline upload .buildkite/block.full.yml
+fi
