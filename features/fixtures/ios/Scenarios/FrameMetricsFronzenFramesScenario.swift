e7887bc6a328c245f1aaa41d0e922265e9cb5b0b
Karl Stenerud <kstenerud@gmail.com>
2024-12-17 10:45:33 +0100

    Add debug statements to FrameMetricsFronzenFramesScenario

--- features/fixtures/ios/Scenarios/FrameMetricsFronzenFramesScenario.swift
+++ features/fixtures/ios/Scenarios/FrameMetricsFronzenFramesScenario.swift
@@ -18,16 +18,27 @@ class FrameMetricsFronzenFramesScenario: Scenario {
     }
     
     override func run() {
+        logInfo("### START")
         let span = BugsnagPerformance.startSpan(name: "FrameMetricsFronzenFramesScenario")
+        logInfo("### SPAN CREATED")
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            logInfo("### SLEEPING FOR 0.3")
             Thread.sleep(forTimeInterval: 0.3)
+            logInfo("### SLEPT FOR 0.3")
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                logInfo("### SLEEPING FOR 0.8")
                 Thread.sleep(forTimeInterval: 0.8)
+                logInfo("### SLEPT FOR 0.8")
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                    logInfo("### SLEEPING FOR 0.4")
                     Thread.sleep(forTimeInterval: 0.4)
+                    logInfo("### SLEPT FOR 0.4")
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                        logInfo("### SLEEPING FOR 1.0")
                         Thread.sleep(forTimeInterval: 1.0)
+                        logInfo("### SLEEPING FOR 1.0")
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                            logInfo("### ENDING SPAN")
                             span.end()
                         }
                     }
