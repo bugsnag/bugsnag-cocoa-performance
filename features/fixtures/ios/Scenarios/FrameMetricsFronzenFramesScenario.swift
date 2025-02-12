393f57fd0659ed11439b77eef5929052e62493d0
Karl Stenerud <kstenerud@gmail.com>
2025-02-12 10:06:23 +0000

    testing

--- features/fixtures/ios/Scenarios/FrameMetricsFronzenFramesScenario.swift
+++ features/fixtures/ios/Scenarios/FrameMetricsFronzenFramesScenario.swift
@@ -18,16 +18,22 @@ class FrameMetricsFronzenFramesScenario: Scenario {
     }
     
     override func run() {
+        logError("###### START")
         let span = BugsnagPerformance.startSpan(name: "FrameMetricsFronzenFramesScenario")
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            logError("###### 1")
             Thread.sleep(forTimeInterval: 0.3)
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                logError("###### 2")
                 Thread.sleep(forTimeInterval: 0.8)
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                    logError("###### 3")
                     Thread.sleep(forTimeInterval: 0.4)
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                        logError("###### 4")
                         Thread.sleep(forTimeInterval: 1.0)
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                            logError("###### END")
                             span.end()
                         }
                     }
