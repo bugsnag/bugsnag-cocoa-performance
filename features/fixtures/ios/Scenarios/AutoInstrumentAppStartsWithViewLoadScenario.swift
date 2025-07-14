981bd8df36a4a0945c6ab38760dd5d03fdf2641d
Robert Bartoszewski <robert.smartbear@gmail.com>
2025-07-14 23:28:48 +0200

    Updated new test scenario

--- features/fixtures/ios/Scenarios/AutoInstrumentAppStartsWithViewLoadScenario.swift
+++ features/fixtures/ios/Scenarios/AutoInstrumentAppStartsWithViewLoadScenario.swift
@@ -10,14 +10,14 @@ import BugsnagPerformance
 @objcMembers
 class AutoInstrumentAppStartsWithViewLoadScenario: Scenario {
     
-    override func configure() {
-        super.configure()
-        config.autoInstrumentAppStarts = true
-        config.autoInstrumentViewControllers = true
+    override func setInitialBugsnagConfiguration() {
+        super.setInitialBugsnagConfiguration()
+        bugsnagPerfConfig.autoInstrumentAppStarts = true
+        bugsnagPerfConfig.autoInstrumentViewControllers = true
         // This test can generate a variable number of spans depending on the OS version,
         // so use a timed send instead.
-        config.internal.autoTriggerExportOnBatchSize = 100
-        config.internal.performWorkInterval = 1
+        bugsnagPerfConfig.internal.autoTriggerExportOnBatchSize = 100
+        bugsnagPerfConfig.internal.performWorkInterval = 1
     }
 
     override func run() {
