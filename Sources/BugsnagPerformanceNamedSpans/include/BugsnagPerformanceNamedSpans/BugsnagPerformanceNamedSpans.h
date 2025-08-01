6d53fab830a8fc5aa4e81be7ba54dcba017a61d2
Robert Bartoszewski <robert.smartbear@gmail.com>
2025-08-01 16:02:52 +0200

    Fixed BugsnagPerformanceNamedSpans.h

--- Sources/BugsnagPerformanceNamedSpans/include/BugsnagPerformanceNamedSpans/BugsnagPerformanceNamedSpans.h
+++ Sources/BugsnagPerformanceNamedSpans/include/BugsnagPerformanceNamedSpans/BugsnagPerformanceNamedSpans.h
@@ -1,11 +1,11 @@
 //
 //  BugsnagPerformanceNamedSpans.h
-//  BugsnagPerformance
+//  BugsnagPerformanceNamedSpans
 //
 //  Created by Robert Bartoszewski on 01/08/2025.
 //  Copyright © 2025 Bugsnag. All rights reserved.
 //
 
 
-#import <BugsnagPerformance/BugsnagPerformanceNamedSpansPlugin.h>
-#import <BugsnagPerformance/BugsnagPerformanceNamedSpanQuery.h>
+#import <BugsnagPerformanceNamedSpans/BugsnagPerformanceNamedSpansPlugin.h>
+#import <BugsnagPerformanceNamedSpans/BugsnagPerformanceNamedSpanQuery.h>
