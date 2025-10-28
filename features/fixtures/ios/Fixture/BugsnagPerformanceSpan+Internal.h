207594f835c8135bcdece185ca8c992531b2d6e8
Robert Bartoszewski <126675445+robert-smartbear@users.noreply.github.com>
2025-10-28 13:04:35 +0100

    Calling endOnDestroy on a span causes a crash in sendCurrentBatchTask (#498)
    * Fixed spanOnDestroy causing a crash
    
    * E2E test
    
    * Updated changelog
    
    ---------
    
    Co-authored-by: Robert Bartoszewski <robert.smartbear@gmail.com>

---
+++ features/fixtures/ios/Fixture/BugsnagPerformanceSpan+Internal.h
@@ -0,0 +1,12 @@
+//
+//  BugsnagPerformanceSpan+Internal.h
+//  Fixture
+//
+//  Created by Robert Bartoszewski on 27/10/2025.
+//
+
+#import <BugsnagPerformance/BugsnagPerformanceSpan.h>
+
+@interface BugsnagPerformanceSpan ()
+- (void)endOnDestroy;
+@end
